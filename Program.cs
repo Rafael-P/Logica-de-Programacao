16f9d6f9ae0cbb6d27584b03623bc01f496150fe
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-11 11:58:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Logica_de_Programação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Entrada de dados
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("--------------------");
+            Console.Write("Digite o primeiro valor: ");
+            int valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            int valorDois = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int soma = valorUm + valorDois;
+
+            //Exibir
+            Console.WriteLine("A soma de "+valorUm+" + "+valorDois+" = "+soma);
+
+        }
+    }
+}
